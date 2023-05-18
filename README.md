7088a3449e284c5b4277e0b49c4c52e26e499c6d
KhalilFreitas <119674471+KhalilFreitasIII@users.noreply.github.com>
2023-05-18 18:45:08 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# ConsultaPokemon
+Site com Front-End básico com HTML e CSS Utilizando da API PokéAPI, criar um sistema de consulta de do nome, número da pokedéx e imagem frontal padrão
---
+++ script.js
@@ -0,0 +1,44 @@
+var input  = document.getElementById("Input")
+
+function title(str) {
+    str = str.toLowerCase();
+    str = str.split(" ");
+    for (var i = 0; i < str.length; i++) 
+      str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);   
+    var str = str.join(" ");
+    return str;
+  }
+  
+async function dadosPokemon(nome) {
+    const urlPokemon = `https://pokeapi.co/api/v2/pokemon/${nome}/`;
+    let ans = ['null',0,'null']
+    try {
+      const response = await fetch(urlPokemon);
+      const data = await response.json();
+      ans[0] = data.name
+      ans[1] = data.id
+      ans[2] = data.sprites.front_default
+      return ans;
+    } catch (error) {
+      console.log("Erro de requerimento", error);
+      return ans;
+    }
+  }
+
+input.addEventListener("keydown",async function(event){
+    if(event.key=="Enter"){
+        event.preventDefault()
+        const pokemon = await dadosPokemon(input.value.toLowerCase().trim())
+        let name = document.getElementById("nome")
+        let id  = document.getElementById('id')
+        let front = document.createElement('img')
+        name.classList.remove("hidden")
+        id.classList.remove("hidden")
+        id.innerHTML = "Pokedex: "+pokemon[1] 
+        name.innerHTML = title(pokemon[0])
+        front.src = pokemon[2]
+        name.appendChild(front);
+        input.value=''
+    }
+})
+
